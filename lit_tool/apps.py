1894e0fefe7249697495fe498a6b5f3e6c0a22bc
Chee Wai Lee <cheewai1972@gmail.com>
2019-05-20 18:10:33 +0800

    Literature Tool content initial commit. This is just the plain template create, and will need to be populated with literature management specific features in the future.

---
+++ lit_tool/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class LitToolConfig(AppConfig):
+    name = 'lit_tool'
---
+++ lit_tool/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path, include
+
+from django.contrib.auth import views as auth_views
+from lit_tool.views import MainView, SecureContentView
+
+urlpatterns = [
+    path('', MainView.as_view(), name='main'),
+    path('secure/', SecureContentView.as_view(), name='secure'),
+    path('login/', auth_views.LoginView.as_view(template_name='lit_tool/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='main'), name='logout'),
+    path('', include('django.contrib.auth.urls')),
+]
